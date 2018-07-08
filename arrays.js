fcedd76a20332cb4fc2c93663fa0c647edc86062
Bluebird Vine <zindzi.thompson@gmail.com>
2018-07-08 22:48:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -20,3 +20,7 @@ function destructivelyAddElementToEndOfArray (array, element){
   array.push(element);
   return array
 }
+
+function accessElementInArray (array, index) {
+  return array[index]
+}
